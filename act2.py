a952c85a32c24f70aa36d340d81edcaf10edd53c
mirmma9-maker <mir.mma9@gmail.com>
2026-04-21 16:11:28 +0300

    act2 and act3 done

---
+++ act2.py
@@ -0,0 +1,13 @@
+#print a sentence 
+print("Welcome to the world of programming!")
+
+#print a Number
+print(4)
+
+#Next line after print
+print("hello python!")
+
+#Print all the keywords present in python
+print("The keywords in python are..... ")
+import keyword
+print(keyword.kwlist)
---
+++ act3.py
@@ -0,0 +1,7 @@
+print("hello world!")
+
+print("my name is rayhan")
+
+print("i am learning python")
+
+print("The result of 2 + 3 is ",(2 + 3))
